a383f1ad3797f7ae62bd3a3d80188c18783a57a8
Rubeno24 <114692745+Rubeno24@users.noreply.github.com>
2022-09-29 11:28:10 -0700

    Create Assignement1.cpp

---
+++ Assignement1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+  
+  cout<<"Greetings - this is CSP 31A:"<<endl; 
+  cout<<"----------------------------"<<endl;
+  cout<<"Ruben Ortega"<<endl;
+  cout<<"Lodi"<<endl;
+   return 0;
+}
